f5d9f31e9aeb04bc48b7f8ddba2e91671e50df03
ucvhuk <ucvhuk@gmail.com>
2022-11-01 14:41:49 +0100

    close menu by click

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,25 +1,28 @@
 (() => {
-	const mobileMenu = document.querySelector("[data-menu]");
-	const menuOpen = document.querySelector("[data-menu-open]");
-	const menuClose = document.querySelector("[data-menu-close]");
-	const bodyLock = document.querySelector("[data-body-lock]");
+  const mobileMenu = document.querySelector('[data-menu]');
+  const menuOpen = document.querySelector('[data-menu-open]');
+  const menuClose = document.querySelector('[data-menu-close]');
+  const bodyLock = document.querySelector('[data-body-lock]');
+  const closebyLink = document.querySelector('.menu__link');
 
-	const toggleMenu = () => {
-		const isMenuOpen = menuOpen.getAttribute("aria-expanded") === "true" || false;
-		menuOpen.setAttribute("aria-expanded", !isMenuOpen);
-		mobileMenu.classList.toggle("is-open");
-		menuOpen.classList.toggle("is-open");
-		bodyLock.classList.toggle("locked");
-	};
+  const toggleMenu = () => {
+    const isMenuOpen =
+      menuOpen.getAttribute('aria-expanded') === 'true' || false;
+    menuOpen.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+    menuOpen.classList.toggle('is-open');
+    bodyLock.classList.toggle('locked');
+  };
 
-	menuOpen.addEventListener("click", toggleMenu);
-	menuClose.addEventListener("click", toggleMenu);
+  menuOpen.addEventListener('click', toggleMenu);
+  menuClose.addEventListener('click', toggleMenu);
+  closebyLink.addEventListener('click', toggleMenu);
 
-	window.matchMedia("(min-width: 768px)").addEventListener("change", e => {
-		if (!e.matches) return;
-		mobileMenu.classList.remove("is-open");
-		menuOpen.classList.remove("is-open");
-		bodyLock.classList.remove("locked");
-		menuOpen.setAttribute("aria-expanded", false);
-	});
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    menuOpen.classList.remove('is-open');
+    bodyLock.classList.remove('locked');
+    menuOpen.setAttribute('aria-expanded', false);
+  });
 })();
