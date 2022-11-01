2b8abfb2c3c96c780f56b139d3d03d4ee456f2d4
uvchuk <110484143+uvchuk@users.noreply.github.com>
2022-11-01 09:07:39 +0000

    Deploying to gh-pages from @ uvchuk/team-project-foodsta@538ba1e931240a61baec0345e59f7a81d9212794 🚀

---
+++ index.2e767ee8.js
@@ -0,0 +1,2 @@
+$(".slider").slick({dots:!1,infinite:!1,speed:300,slidesToShow:1,adaptiveHeight:!1});
+//# sourceMappingURL=index.2e767ee8.js.map
---
+++ index.4d58f023.js
@@ -0,0 +1,2 @@
+$(".slider").slick({dots:!1,infinite:!1,speed:300,slidesToShow:1,adaptiveHeight:!1});
+//# sourceMappingURL=index.4d58f023.js.map
