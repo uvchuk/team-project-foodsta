94356e9c3684330d7e0dc9a4e3b1fb27d9d51c14
ucvhuk <ucvhuk@gmail.com>
2022-11-01 14:09:27 +0100

    add scroll script

---
+++ src/js/scroll.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('a[href^="#"').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    let href = this.getAttribute('href').substring(1);
+
+    const scrollTarget = document.getElementById(href);
+
+    const topOffset = document.querySelector('.page-header').offsetHeight;
+    const elementPosition = scrollTarget.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - topOffset;
+
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: 'smooth',
+    });
+  });
+});
